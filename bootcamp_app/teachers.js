9886534e0963d5e51792b7d1272aa44c71e19972
Karina Song <ksysong@gmail.com>
2021-04-01 04:22:41 +0000

    parametize student and teacher queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+// Have complete control over queryString
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers on teacher_id = teachers.id
 JOIN students on student_id = students.id
 JOIN cohorts on cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-}).catch(err => console.error('query error', err.stack));
+`;
+
+// storing the value that comes from outside
+const value = [process.argv[2] || 'JUL02'];
+
+pool
+  .query(queryString, value)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    })
+  }).catch(err => console.error('query error', err.stack));
