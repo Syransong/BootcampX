b03e05257105cee3969d5884387e924d10ce91bc
Karina Song <ksysong@gmail.com>
2021-04-01 03:26:35 +0000

    add query to see student id, student name and cohort

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,3 +6,15 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+
+pool.query(`
+SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+LIMIT 5;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+});
